3f580d8c8eb4f8c85055a1e29c894727fa7827eb
fengmk2 <m@fengmk2.com>
2016-07-29 18:00:08 +0800

    feat: support mocha custom require args

--- test/egg-test.test.js
+++ test/egg-test.test.js
@@ -1,36 +1,33 @@
 'use strict';
 
 const path = require('path');
-const assert = require('assert');
+const assert = require('power-assert');
 const coffee = require('coffee');
 const mm = require('mm');
 
 describe('egg-bin test', () => {
   const eggBin = require.resolve('../bin/egg-bin.js');
+  const cwd = path.join(__dirname, 'fixtures/test-files');
 
   afterEach(mm.restore);
 
   it('should success', done => {
     mm(process.env, 'TESTS', 'test/**/*.test.js');
-    coffee.fork(eggBin, [ 'test' ], {
-      cwd: path.join(__dirname, 'fixtures/test-files'),
-    })
+    coffee.fork(eggBin, [ 'test' ], { cwd })
     .expect('stdout', /✓ should success/)
     .expect('stdout', /a\.test\.js/)
     .expect('stdout', /b\/b\.test\.js/)
     .expect('code', 0)
     .end((err, res) => {
       assert.ifError(err);
-      assert.ok(!/a.js/.test(res.stdout));
+      assert.ok(!/a\.js/.test(res.stdout));
       done();
     });
   });
 
   it('should only test files specified by TESTS', done => {
     mm(process.env, 'TESTS', 'test/a.test.js');
-    coffee.fork(eggBin, [ 'test' ], {
-      cwd: path.join(__dirname, 'fixtures/test-files'),
-    })
+    coffee.fork(eggBin, [ 'test' ], { cwd })
     .expect('stdout', /✓ should success/)
     .expect('stdout', /a\.test\.js/)
     .expect('code', 0)
@@ -44,9 +41,7 @@ describe('egg-bin test', () => {
   it('should use process.env.TEST_REPORTER', done => {
     mm(process.env, 'TESTS', 'test/**/*.test.js');
     mm(process.env, 'TEST_REPORTER', 'dot');
-    coffee.fork(eggBin, [ 'test' ], {
-      cwd: path.join(__dirname, 'fixtures/test-files'),
-    })
+    coffee.fork(eggBin, [ 'test' ], { cwd })
     .expect('stdout', /․․\n/)
     .expect('code', 0)
     .end((err, res) => {
@@ -59,11 +54,21 @@ describe('egg-bin test', () => {
   it('should use process.env.TEST_TIMEOUT', done => {
     mm(process.env, 'TESTS', 'test/**/*.test.js');
     mm(process.env, 'TEST_TIMEOUT', '60000');
-    coffee.fork(eggBin, [ 'test' ], {
-      cwd: path.join(__dirname, 'fixtures/test-files'),
-    })
+    coffee.fork(eggBin, [ 'test' ], { cwd })
     .expect('stdout', /✓ should success/)
     .expect('code', 0)
+    .end(done);
+  });
+
+  it('should fail when test fail with power-assert', done => {
+    mm(process.env, 'TESTS', 'test/power-assert-fail.js');
+    coffee.fork(eggBin, [ 'cov', '-r', 'intelli-espower-loader' ], { cwd })
+    .coverage(false)
+    // .debug()
+    .expect('stdout', /1\) should fail/)
+    .expect('stdout', /assert\(1 === 2\)/)
+    .expect('stdout', /1 failing/)
+    .expect('code', 1)
     .end(done);
   });
 
--- test/my-egg-bin.test.js
+++ test/my-egg-bin.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const path = require('path');
-const assert = require('assert');
+const assert = require('power-assert');
 const coffee = require('coffee');
 const mm = require('mm');
 
