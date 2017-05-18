022cb132cc3cff7297690a472cd4148edeecc88d
popomore <sakura9515@gmail.com>
2017-05-18 15:21:14 +0800

    feat(cov): add prerequire option

--- test/lib/cmd/cov.test.js
+++ test/lib/cmd/cov.test.js
@@ -114,6 +114,21 @@ describe('test/lib/cmd/cov.test.js', () => {
           .expect('code', 1)
           .end(done);
       });
+
+      it('should set EGG_BIN_PREREQUIRE', function* () {
+        const cwd = path.join(__dirname, '../../fixtures/prerequire');
+        yield coffee.fork(eggBin, [ 'cov' ], { cwd })
+          .debug()
+          .expect('stdout', /EGG_BIN_PREREQUIRE undefined/)
+          .expect('code', 0)
+          .end();
+
+        yield coffee.fork(eggBin, [ 'cov', '--prerequire' ], { cwd })
+          .debug()
+          .expect('stdout', /EGG_BIN_PREREQUIRE true/)
+          .expect('code', 0)
+          .end();
+      });
     });
   } else {
     it('should exec test instead of cov in win32', done => {
