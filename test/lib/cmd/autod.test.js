e0b9359fa26a050ceb6a829dd704bda17e79dc7a
fengmk2 <fengmk2@gmail.com>
2017-08-01 00:34:21 +0800

    f

---
+++ test/lib/cmd/autod.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const path = require('path');
+const coffee = require('coffee');
+
+const eggBin = require.resolve('../../../bin/egg-bin.js');
+
+describe('test/lib/cmd/autod.test.js', () => {
+  it('should autod check fail', function* () {
+    const cwd = path.join(__dirname, '../../fixtures/autod-missing');
+    yield coffee.fork(eggBin, [ 'autod', '--check' ], { cwd })
+      .debug()
+      .expect('code', 1)
+      .expect('stderr', /\[ERROR\] Missing dependencies: \["urllib"\]/)
+      .end();
+  });
+
+  it('should autod check pass', function* () {
+    const cwd = path.join(__dirname, '../../fixtures/autod-exists');
+    yield coffee.fork(eggBin, [ 'autod', '--check' ], { cwd })
+      .debug()
+      .expect('code', 0)
+      .end();
+  });
+});
