3e8ce0d3aaaea793f466b768f45f77e4fcc7b001
TZ <atian25@qq.com>
2017-09-05 14:31:05 +0800

    refactor: use common-bin parse execArgv

--- test/fixtures/my-egg-bin/lib/cmd/echo.js
+++ test/fixtures/my-egg-bin/lib/cmd/echo.js
@@ -8,7 +8,10 @@ class EchoCommand extends Command {
   }
 
   * run(context) {
-    console.log('%j', context);
+    console.log('argv: %j', context.argv);
+    console.log('debugPort: %s', context.debugPort);
+    console.log('debugOptions: %j', context.debugOptions);
+    console.log('execArgv: %j', context.execArgv);
   }
 }
 
