db030d7a1f2f42c569c07d92b8ddcf7b7eb7edac
TZ <atian25@qq.com>
2018-02-11 09:37:10 +0800

    feat: --baseDir

--- test/lib/cmd/check.test.js
+++ test/lib/cmd/check.test.js
@@ -17,4 +17,16 @@ describe('test/lib/cmd/check.test.js', () => {
       .expect('code', 1)
       .end();
   });
+
+  it('should check eggache with --baseDir', function* () {
+    const cwd = path.join(__dirname, '../../fixtures/check-eggache');
+    yield coffee.fork(eggBin, [ 'check', '--baseDir', cwd ])
+      // .debug()
+      .expect('stdout', /eggache\/no-override-exports/)
+      .expect('stdout', /eggache\/no-unexpected-plugin-keys/)
+      .notExpect('stdout', /no-unused-vars/)
+      .notExpect('stdout', /\/other.js/)
+      .expect('code', 1)
+      .end();
+  });
 });
