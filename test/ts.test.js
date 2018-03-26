ef2b479f9927ed7cc283d859540f8a6c43a94cda
TZ <atian25@qq.com>
2018-03-26 22:09:14 +0800

    test: add egg2 test && real cluster test

--- test/ts.test.js
+++ test/ts.test.js
@@ -6,11 +6,12 @@ const mm = require('mm');
 
 describe('test/ts.test.js', () => {
   const eggBin = require.resolve('../bin/egg-bin');
-  const cwd = path.join(__dirname, './fixtures/ts');
+  let cwd;
 
   afterEach(mm.restore);
 
   it('should support ts', () => {
+    cwd = path.join(__dirname, './fixtures/ts');
     mm(process.env, 'NODE_ENV', 'development');
     return coffee.fork(eggBin, [ 'dev', '--typescript' ], { cwd })
       .debug()
@@ -22,6 +23,7 @@ describe('test/ts.test.js', () => {
   });
 
   it('should support ts test', () => {
+    cwd = path.join(__dirname, './fixtures/ts');
     mm(process.env, 'NODE_ENV', 'development');
     return coffee.fork(eggBin, [ 'test', '--typescript' ], { cwd })
       .debug()
@@ -29,6 +31,20 @@ describe('test/ts.test.js', () => {
       .notExpect('stdout', /should not load js files/)
       .expect('stdout', /--- \[string\] 'wrong assert ts'/)
       .expect('code', 1)
+      .end();
+  });
+
+  it('should start app', () => {
+    if (process.env.EGG_VERSION && process.env.EGG_VERSION === '1') { // eslint-disable-line eqeqeq
+      console.log('skip egg@1');
+      return;
+    }
+    cwd = path.join(__dirname, './fixtures/example-ts');
+    return coffee.fork(eggBin, [ 'dev', '--ts' ], { cwd })
+      .debug()
+      .expect('stdout', /hi, egg, 12345/)
+      .expect('stdout', /egg started/)
+      .expect('code', 0)
       .end();
   });
 });
