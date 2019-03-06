742dcbe6d29ee6c2c43afcc8379043e8a9f92b5e
wanghx <whx89768@alibaba-inc.com>
2019-03-06 18:27:41 +0800

    f

--- test/ets-bin.test.js
+++ test/ets-bin.test.js
@@ -2,12 +2,19 @@
 
 const path = require('path');
 const coffee = require('coffee');
+const semver = require('semver');
 
 describe('test/ets-bin.test.js', () => {
   const etsBin = require.resolve('../bin/ets.js');
   const cwd = path.join(__dirname, 'fixtures/example-ts-ets');
 
   it('should test with ets', () => {
+    const higherVersion = semver.gte(process.version, '8.0.0');
+    if (!higherVersion) {
+      // skip 6.x, egg-ts-helper only works in >=8.0.0
+      return;
+    }
+
     return coffee.fork(etsBin, [], {
       cwd,
       env: Object.assign({}, process.env, {
