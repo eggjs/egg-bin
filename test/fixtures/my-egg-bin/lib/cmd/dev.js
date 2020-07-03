fcdccd7b3fc9bef87c0c4dace4ada4d8b30922a9
TZ <atian25@qq.com>
2020-07-03 10:10:02 +0800

    feat: expose proc

--- test/fixtures/my-egg-bin/lib/cmd/dev.js
+++ test/fixtures/my-egg-bin/lib/cmd/dev.js
@@ -8,6 +8,9 @@ class MyDevCommand extends DevCommand {
     // find your framework
     const yadan = path.join(__dirname, '../../../custom-framework-app/node_modules/yadan');
     context.argv.framework = yadan;
+    setTimeout(() => {
+      console.log('proc: %s', this.proc.pid);
+    }, 1000);
     yield super.run(context);
   }
 }
