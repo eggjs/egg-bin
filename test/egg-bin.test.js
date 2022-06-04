ee594c7e36342531c10ad25c1fd14fe4346a7945
fengmk2 <fengmk2@gmail.com>
2022-06-04 11:30:07 +0800

    📦 NEW: Support run test with node:test
    ```bash
    egg-bin node-test
    ```

--- test/egg-bin.test.js
+++ test/egg-bin.test.js
@@ -8,28 +8,28 @@ describe('test/egg-bin.test.js', () => {
   const cwd = path.join(__dirname, 'fixtures/test-files');
 
   describe('global options', () => {
-    it('should show version', done => {
-      coffee.fork(eggBin, [ '--version' ], { cwd })
+    it('should show version', () => {
+      return coffee.fork(eggBin, [ '--version' ], { cwd })
         // .debug()
         .expect('stdout', /\d+\.\d+\.\d+/)
         .expect('code', 0)
-        .end(done);
+        .end();
     });
 
-    it('should show help', done => {
-      coffee.fork(eggBin, [ '--help' ], { cwd })
+    it('should show help', () => {
+      return coffee.fork(eggBin, [ '--help' ], { cwd })
       // .debug()
         .expect('stdout', /Usage: .*egg-bin.* \[command] \[options]/)
         .expect('code', 0)
-        .end(done);
+        .end();
     });
 
-    it('should show help when command not exists', done => {
-      coffee.fork(eggBin, [ 'not-exists' ], { cwd })
+    it('should show help when command not exists', () => {
+      return coffee.fork(eggBin, [ 'not-exists' ], { cwd })
       // .debug()
         .expect('stdout', /Usage: .*egg-bin.* \[command] \[options]/)
         .expect('code', 0)
-        .end(done);
+        .end();
     });
   });
 });
---
+++ test/node-test.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const path = require('path');
+const coffee = require('coffee');
+
+describe('test/node-test.test.js', () => {
+  const eggBin = require.resolve('../bin/egg-bin.js');
+  const cwd = path.join(__dirname, 'fixtures/node-test');
+
+  it('should run with node-test mode work', () => {
+    return coffee.fork(eggBin, [ 'node-test' ], { cwd })
+      // .debug()
+      .expect('stdout', /# tests 2/)
+      .expect('stdout', /# pass 1/)
+      .expect('stdout', /# fail 1/)
+      .expect('code', 1)
+      .end();
+  });
+});
