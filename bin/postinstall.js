0538e6f0e75e8f021a684aa56c7dfa20c3ba6704
fengmk2 <fengmk2@gmail.com>
2023-01-06 12:05:19 +0800

    fix: ignore egg module on ets

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -3,6 +3,9 @@
 const path = require('path');
 const fs = require('fs');
 
+// node posintall.js <egg-ts-helper/dist/bin>
+const etsBinName = process.argv[2] || 'egg-ts-helper/dist/bin';
+
 // try to use INIT_CWD env https://docs.npmjs.com/cli/v9/commands/npm-run-script
 // npm_rootpath is npminstall
 const npmRunRoot = process.env.INIT_CWD || process.env.npm_rootpath;
@@ -10,10 +13,11 @@ if (npmRunRoot) {
   const pkgFile = path.join(npmRunRoot, 'package.json');
   if (fs.existsSync(pkgFile)) {
     const pkg = require(pkgFile);
-    if (pkg.egg && pkg.egg.typescript) {
+    // ignore eggModule
+    if (!pkg.eggModule && pkg.egg && pkg.egg.typescript) {
       // set ETS_CWD
       process.env.ETS_CWD = npmRunRoot;
-      require('egg-ts-helper/dist/bin');
+      require(etsBinName);
     }
   }
 }
