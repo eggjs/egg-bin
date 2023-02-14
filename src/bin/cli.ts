9f78724dc617ae5ba919e1183ec325ad070e2971
fengmk2 <fengmk2@gmail.com>
2023-02-14 09:18:21 +0800

    f

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
+import path from 'node:path';
 import { start } from '@artus-cli/artus-cli';
 
-start({
-  exclude: [ 'scripts', 'bin', 'test', 'coverage' ],
-});
+const isDist = path.basename(path.dirname(__dirname)) === 'dist';
+const exclude = [ 'scripts', 'bin', 'test', 'coverage' ];
+if (isDist) {
+  exclude.push('src');
+}
+
+start({ exclude });
