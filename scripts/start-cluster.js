aa718876268f2e74221700408f9d69a29f073979
fengmk2 <fengmk2@gmail.com>
2023-02-16 14:20:17 +0800

    f

--- scripts/start-cluster.js
+++ scripts/start-cluster.js
@@ -1,6 +1,11 @@
 const debug = require('util').debuglog('egg-bin:lib:start-cluster');
 
 debug('argv: %o', process.argv);
-const options = JSON.parse(process.argv[2]);
+let optionsJSONString = process.argv[2];
+if (process.platform === 'win32' && optionsJSONString.startsWith('\'')) {
+  // '{"baseDir":" => {"baseDir":
+  optionsJSONString = optionsJSONString.startsWith(1, optionsJSONString.length - 1);
+}
+const options = JSON.parse(optionsJSONString);
 debug('start cluster options: %o', options);
 require(options.framework).startCluster(options);
