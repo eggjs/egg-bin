b33f3099c705429e4810af228bedef3507d01e77
killa <killa123@126.com>
2024-04-19 09:17:06 +0800

    feat: ignore ts error in dalgen (#264)
    <!--
    Thank you for your pull request. Please review below requirements.
    Bug fixes and new features should include tests and possibly benchmarks.
    Contributors guide:
    https://github.com/eggjs/egg/blob/master/CONTRIBUTING.md
    
    感谢您贡献代码。请确认下列 checklist 的完成情况。
    Bug 修复和新功能必须包含测试，必要时请附上性能测试。
    Contributors guide:
    https://github.com/eggjs/egg/blob/master/CONTRIBUTING.md
    -->
    
    ##### Checklist
    <!-- Remove items that do not apply. For completed items, change [ ] to
    [x]. -->
    
    - [ ] `npm test` passes
    - [ ] tests and/or benchmarks are included
    - [ ] documentation is changed or added
    - [ ] commit message follows commit guidelines
    
    ##### Affected core subsystem(s)
    <!-- Provide affected core subsystem(s). -->
    
    
    ##### Description of change
    <!-- Provide a description of the change below this comment. -->

--- test/lib/cmd/dal.test.js
+++ test/lib/cmd/dal.test.js
@@ -7,18 +7,53 @@ const assert = require('assert');
 describe('test/lib/cmd/dal.test.js', () => {
   const eggBin = require.resolve('../../../bin/egg-bin.js');
   const cwd = path.join(__dirname, '../../fixtures/dal');
+  const cwd2 = path.join(__dirname, '../../fixtures/dal-with-ts-error');
 
   afterEach(mm.restore);
 
   describe('egg-bin dal gen', () => {
     after(async () => {
       await fs.rm(path.join(cwd, 'app/modules/dal/dal'), {
+        force: true,
+        recursive: true,
+      });
+      await fs.rm(path.join(cwd2, 'app/modules/dal/dal'), {
+        force: true,
         recursive: true,
       });
     });
 
     it('egg-bin dal gen should work', async () => {
       await coffee.fork(eggBin, [ 'dal', 'gen', '--teggPkgName', '@eggjs/xianyadan', '--teggDalPkgName', '@eggjs/xianyadan/dal' ], { cwd })
+        .debug()
+        .expect('code', 0)
+        .end();
+
+      for (const file of [
+        'app/modules/dal/dal/dao/BarDAO.ts',
+        'app/modules/dal/dal/dao/FooDAO.ts',
+        'app/modules/dal/dal/dao/base/BaseBarDAO.ts',
+        'app/modules/dal/dal/dao/base/BaseFooDAO.ts',
+        'app/modules/dal/dal/extension/BarExtension.ts',
+        'app/modules/dal/dal/extension/FooExtension.ts',
+        'app/modules/dal/dal/structure/Bar.json',
+        'app/modules/dal/dal/structure/Bar.sql',
+        'app/modules/dal/dal/structure/Foo.json',
+        'app/modules/dal/dal/structure/Foo.sql',
+      ]) {
+        assert.ok(await fs.stat(path.join(cwd, file)));
+      }
+
+      const content = await fs.readFile(path.join(cwd, 'app/modules/dal/dal/dao/base/BaseFooDAO.ts'), 'utf8');
+      assert(/import type { InsertResult, UpdateResult, DeleteResult } from '@eggjs\/xianyadan\/dal';/.test(content));
+      assert(/import { SingletonProto, AccessLevel, Inject } from '@eggjs\/xianyadan';/.test(content));
+    });
+
+    it('egg-bin dal gen with ts error should work', async () => {
+      const cwd = path.join(__dirname, '../../fixtures/dal-with-ts-error');
+      await coffee.fork(eggBin, [ 'dal', 'gen', '--teggPkgName', '@eggjs/xianyadan', '--teggDalPkgName', '@eggjs/xianyadan/dal' ], {
+        cwd: cwd2,
+      })
         .debug()
         .expect('code', 0)
         .end();
